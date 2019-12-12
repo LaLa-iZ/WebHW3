027456f84e90af53c56d2f61733377f1b3160265
Анастасия Ларионова <larionova.ai@students.dvfu.ru>
2019-12-12 23:40:47 +1000

    task 2

---
+++ 3hw_2/Startup.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace _3hw_2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            int x = 127;
+            int y = 3;
+            int z = 0;
+
+            app.Use(async (context, next) =>
+            {
+                z = ((x * x) + (4 * x)) * ((y + 7) * (y + 7));
+                await next.Invoke();
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"y = (x^2 + 4x)(a + 7)^2, ({x}^2 + 4*{x})({y} + 7)^2 = {z} \n");
+            });
+        }
+    }
+}
